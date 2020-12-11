70e8b523f9db575bde5b93ac33cc2ed5984bd498
Marat Gumerov <marat.gumerov99@gmail.com>
2020-12-11 11:17:44 +0500

    linter fixes 3

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -19,7 +19,11 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('redoc/', TemplateView.as_view(template_name='redoc.html'), name='redoc'),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
     path('api/v1/', include('yamdb.urls')),
     path('api/v1/', include('users.urls'))
 
